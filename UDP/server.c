a38b8831bc80e4a3332660e910ad294ac0974acb
tomasdanelon <60556407+tomasdanelon@users.noreply.github.com>
2023-06-28 11:00:18 -0300

    Update server.c

--- UDP/server.c
+++ UDP/server.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <time.h>
 
 #define PORT 8080
-#define MAX_BUFFER 1024
+#define MAX_BUFFER 32000
 
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[MAX_BUFFER] = {0};
-    char *message = "Hello from server"; //Substituir aqui pela mensagem
+    char buffer[MAX_BUFFER] = {"Teste de comunicacao"}; //Alterar esse campo para a mensagem desejada
+    clock_t start, end;
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) == 0) {
@@ -32,13 +32,22 @@ int main() {
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Forcefully attaching socket to the port
+    // Binding the socket to the port
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    send(new_socket, message, strlen(message), 0);
-    printf("Message sent\n");
+    // Receiving data and measuring time
+    start = clock();
+    read(new_socket, buffer, MAX_BUFFER);
+    end = clock();
+
+    // Sending data back
+    send(new_socket, buffer, strlen(buffer), 0);
+
+    double time_taken = ((double)end - start) / CLOCKS_PER_SEC;
+    printf("Time taken: %f\n", time_taken);
+
     return 0;
 }
