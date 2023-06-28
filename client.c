14ddc8cf54f4d6901cbd031c0b737dbbf864acc5
tomasdanelon <60556407+tomasdanelon@users.noreply.github.com>
2023-06-28 10:58:30 -0300

    Update client.c

--- client.c
+++ client.c
@@ -4,12 +4,17 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
+
 #define PORT 8080
+#define MAX_BUFFER 32000
 
 int main() {
     int sock = 0;
     struct sockaddr_in serv_addr;
-    char buffer[1024] = {0};
+    char buffer[MAX_BUFFER] = "Hello server"; //Substituir a mensagem aqui
+    clock_t start, end;
+
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
         return -1;
@@ -19,8 +24,7 @@ int main() {
     serv_addr.sin_port = htons(PORT);
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    // Substituir pelo seu IP
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
@@ -30,7 +34,14 @@ int main() {
         return -1;
     }
 
-    read(sock, buffer, 1024);
-    printf("%s\n", buffer);
+    // Sending data and measuring time
+    start = clock();
+    send(sock, buffer, strlen(buffer), 0);
+    read(sock, buffer, MAX_BUFFER);
+    end = clock();
+
+    double time_taken = ((double)end - start) / CLOCKS_PER_SEC;
+    printf("Time taken: %f\n", time_taken);
+
     return 0;
 }
